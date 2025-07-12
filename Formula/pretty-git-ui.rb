85e3e0b1ec2715f9612aada68c33f74f68630107
mimu <48762864+mmrakt@users.noreply.github.com>
2025-07-12 20:47:41 +0900

    Create pretty-git-ui.rb

---
+++ Formula/pretty-git-ui.rb
@@ -0,0 +1,17 @@
+class PrettyGitUi < Formula
+  desc "Terminal UI for git operations"
+  homepage "https://github.com/mmrakt/pretty-git-ui"
+  url "https://github.com/mmrakt/pretty-git-ui/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "ea002bfd275fb23b1d6c5985fd6c607c5bbcf8ba94f71f6fd5667fb9f8814901"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    assert_match "pretty-git-ui", shell_output("#{bin}/pretty-git-ui --version")
+  end
+end
